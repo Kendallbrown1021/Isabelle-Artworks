c6bad56758945a02bd610322daff76821d575da9
Kendallbrown1021 <kendall.darnel.brown@gmail.com>
2022-06-02 14:49:23 -0700

    added contentful connectivity so gallery data can be pulled

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    SPACE_ID: "r874ru59pzq9",
+    ACCESS_TOKEN: "tLYuXaS8282ZMOCKZ5R2HKN9HuaDGKF4dKPcH4K-yqo",
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { createClient } from "contentful";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="container">
+      {console.log(props.posts.items)}
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,5 +21,22 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+  const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+  const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "galleryPhoto" });
+
+  return {
+    props: {
+      posts: res,
+    },
+  };
 }
